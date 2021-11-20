c0cbd84cbab50970bb6a1023be91940003a6bbfb
Makhlug Jafarov <mexluq.2016@gmail.com>
2021-11-20 18:45:13 +0100

    Update tictactoe_basic.cpp

--- tictactoe_basic.cpp
+++ tictactoe_basic.cpp
@@ -1,7 +1,6 @@
 /* 
-*   Copyright (C) 2021-01?? Makhlug Jafarov
+*   Copyright (C) 2020-12 Makhlug Jafarov
 *
-*   This code is made by Me, just as simple as shown below. You can freely take this code and modify if necessary. Glad this code will help you. Regards!
     
     //github.com/jafarov01
     //https://www.linkedin.com/in/jafarov01/
